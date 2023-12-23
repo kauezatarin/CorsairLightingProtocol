53aa68a41088a0c79f82c7084d4087dc3b034856
Kauê Zatarin <kauezatarin@hotmail.com>
2023-12-23 12:42:10 -0300

    teste

--- src/FastLEDController.cpp
+++ src/FastLEDController.cpp
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-#include "FastLEDController.h"
+#include "FastLEDController.h" 
 
 FastLEDController::FastLEDController(FastLEDControllerStorage* storage)
 	: temperatureController(nullptr), storage(storage) {
